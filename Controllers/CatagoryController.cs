8e60863b44b9175982dd1e7e0b60fb1506a8d419
User <User@DESKTOP-D45ILOF>
2025-04-13 11:21:07 +0530

    Add project files.

---
+++ Controllers/CatagoryController.cs
@@ -0,0 +1,23 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CatagoryController : Controller
+    {
+        private readonly ApplicationDBContext _applicationDBContext;
+        public CatagoryController(ApplicationDBContext applicationDBContext)
+        {
+            _applicationDBContext = applicationDBContext;
+        }
+        public IActionResult Index()
+        {
+            var data1 = _applicationDBContext.Category.ToList();
+            return View(data1);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Catagory> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Catagory>().HasData(
+                new Catagory { Id = 1, Name="Action", DisplayOrder = 1},
+                 new Catagory { Id = 2, Name = "Action", DisplayOrder = 2},
+                  new Catagory { Id = 3, Name = "Action", DisplayOrder = 3}
+                );
+
+        }
+
+    }
+}
